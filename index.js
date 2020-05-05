f16a658c1b848f49a5611ae8e7e6d2d559ff8a49
shridhar-t <thakur.shridhar3093@gmail.com>
2020-05-05 09:27:10 +0530

    add sound and modified UI

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var random1=Math.floor((6*Math.random())+1);
-var random2=Math.floor((6*Math.random())+1);
-document.querySelector(".img1").setAttribute("src","./images/dice"+random1+".png");
-document.querySelector(".img2").setAttribute("src","./images/dice"+random2+".png");
-var state1=document.querySelector(".img1").getAttribute("src");
-var state2=document.querySelector(".img2").getAttribute("src");
-if(state1>state2)
-{
-    document.querySelector("h1").textContent="Player 1 Wins";
-}
-else if(state1<state2)
-{
-    document.querySelector("h1").textContent="Player 2 Wins";
-}
-else
-{
-    document.querySelector("h1").textContent="Draw";
-}
+document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
+document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
+document.querySelector(".button").addEventListener("click", () => {
+    var audio = new Audio("./sounds/roll.mp3");
+    audio.play();
+    var random1 = Math.floor((6 * Math.random()) + 1);
+    var random2 = Math.floor((6 * Math.random()) + 1);
+    document.querySelector(".img1").setAttribute("src", "./images/dice" + random1 + ".png");
+    document.querySelector(".img2").setAttribute("src", "./images/dice" + random2 + ".png");
+    var state1 = document.querySelector(".img1").getAttribute("src");
+    var state2 = document.querySelector(".img2").getAttribute("src");
+    if (state1 > state2) {
+        document.querySelector("h1").textContent = "Player 1 Wins";
+    } else if (state1 < state2) {
+        document.querySelector("h1").textContent = "Player 2 Wins";
+    } else {
+        document.querySelector("h1").textContent = "Draw";
+    }
+})
